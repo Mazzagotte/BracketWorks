4ce07455e0609fed363669aa0217be3b46181d3e
Your Name <you@example.com>
2026-05-18 07:21:43 -0600

    More mobile updates

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1267,7 +1267,7 @@ export default function TournamentDashboard() {
                   }
                 }}
               >
-                Dev: Unload Squad
+                Unload Squad
               </button>
             </div>
           )}
