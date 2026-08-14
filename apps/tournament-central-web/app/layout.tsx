1b4def899a8138acd565b6cab4079270ff52b562
James <jamesmazzagotte5@gmail.com>
2026-08-13 19:37:37 -0600

    Fixed andriod Logo

--- apps/tournament-central-web/app/layout.tsx
+++ apps/tournament-central-web/app/layout.tsx
@@ -10,6 +10,8 @@ export const metadata = {
       { url: '/favicon.ico', sizes: 'any' },
       { url: '/icons/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
       { url: '/icons/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/icons/icon-192.png', type: 'image/png', sizes: '192x192' },
+      { url: '/icons/icon-512.png', type: 'image/png', sizes: '512x512' },
     ],
     apple: [{ url: '/icons/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
   },
